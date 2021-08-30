9bd6c438974a1b17bec1a59eb23c051c168734f1
CrystyanSantos9 <cryssantos9@gmail.com>
2021-08-30 00:34:16 +0000

    change theme color

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -1,38 +1,81 @@
 import React, { useState } from "react";
 
 import {FiLink} from 'react-icons/fi'
+import {RiMailLine}from 'react-icons/ri'
 import { FaLinkedin, FaGithub, FaYoutube} from 'react-icons/fa'
 import { ToggleButton } from '../ToggleButton'
+import Image from 'next-images'
 
 const Hero=()=>{
     const [selected, setSelected] = useState(false);
-    return(
-      
-        <div className="pt-32 md:grid md:grid-cols-2 md:pt-16 md:mx-24 leading-none">
+    const[underline, setUnderline] = useState("_");
+    const sendMail = e=>{
+        console.log('enviando email ')
+    }
 
-                <div className="pt-16 md:pt-32">
-         
-                    <h1 className="text-4xl uppercase pl-10">Olá, eu sou o Crystyan Santos</h1>
-                    <h2 className="text-4xl font-bold md:text-5xl uppercase pl-16">Full Stack Developer</h2>
-                    <div className="mx-8 px-8 relative border-yellow border-2 rounded md:mx-0 md:px-16 pb-4 pt-10 mt-6">
-                    <h3 className="text-white absolute bg-yellow py-2 px-6 top-0 -mt-5 text-2xl font-bold uppercase">Contato</h3>
-                       <p className="text-center md:text-left">
-                           <FaLinkedin className="text-5xl md:text-6xl inline-block md:mr-6" />
-                           <FaGithub className="text-5xl md:text-6xl inline-block md:mr-6" />
-                           <FaYoutube className="text-5xl md:text-6xl inline-block md:mr-6" />
-                           <FiLink className="text-5xl md:text-6xl inline-block" />
-                        <br />
-                        <span className="font-bold inline-block mt-4 shadow-inner">Ou deixe uma mensagem: cryssantos9@gmail.com</span>
-                       </p>
-                    </div>
-           
-                    
+    const underlineShowNotShow = (underline) =>{
+   console.log(underline)
+   setTimeout(()=>{
+            if(underline){
+                setUnderline(false)
+            }else{
+                setUnderline('_')
+            }
+        },1200)
+    }
+
+    underlineShowNotShow(underline);
+
+    
+    return(
+
+        <div class="sm:bg-blue-100 md:bg-green-300 lg:bg-red-600 xl:bg-yellow-700 lg:flex lg:flex-row lg:justify-between"
+        style={{
+            backgroundImage: "url('/images/cedric-frixon-QiJp6dWw1oE-unsplash.jpg')",
+            width: "100%",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover"
+        }}
+        >
+
+   
+
+                <div className="pt-16 md:pt-32  lg:w-3/6 lg:p-10 ">
+                    <h1 className="text-4xl uppercase pl-16 lg:text-4xl">Olá, eu sou o Crystyan Santos</h1>
+                    <h2 className="text-3xl font-bold uppercase pl-16 lg:text-4xl lg:pb-10">Javascript dev<span className="text-4xl font-bold uppercase">{underline}</span></h2>
+                    <div className="mx-8 px-8 relative border-blue-600 border-2 rounded md:mx-10  md:px-16 pb-4 pt-10 mt-6 lg:p-16">
+                    <h3 className="text-white absolute bg-blue-600 py-2 px-6 top-0 -mt-5 text-2xl font-bold uppercase lg:text-4xl lg:-mt-5">Contato</h3>
+                       <div className="flex  justify-between flex-wrap">
+                           <div>
+                           <FaLinkedin className="text-5xl sm:text-5xl md:text-5xl" />
+                           </div>
+                           <div>
+                           <FaGithub className="text-5xl sm:text-5xl md:text-5xl"/>
+                           </div>
+                         <div>
+                         <FaYoutube  className="text-5xl sm:text-5xl md:text-5xl"/>
+                         </div>
+                         
+                         <div>
+                         <RiMailLine className="text-5xl sm:text-5xl md:text-5xl" onClick={sendMail}/>
+                         </div>
+
+                       </div>
+                    </div>   
                 </div>
 
-                <div className="my-5  mb-0 sm:pt-48 mx-8 px-8 md:mx-auto"><img src="/images/avatar3.png" /></div>
 
+                <div className="flex flex-row content-center justify-center lg:w-3/6 lg:p-32 lg:pl-24">
+                   
+                    <img  className="w-48 lg:w-48 lg:h-48"src="/images/avatar.png" />
                
+       
+        </div>
+
+{/* fim */}
             </div>
+
+            
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,13 @@ import {GoGist, GoStar} from 'react-icons/go'
 import PageHead from '../components/PageHead'
 import Hero from '../components/Hero'
 import { ToggleButton } from '../components/ToggleButton';
+import {RiArrowDownSLine} from 'react-icons/ri'
 
-
+const customStyles = {
+  content: {
+   background: "#000",
+  },
+}
 
 const Index = ({ repos, user }) => {
 
@@ -45,7 +50,7 @@ const Index = ({ repos, user }) => {
 
     //JSX
     return (
-        <div className="font-sans">
+        <div className="font-sans mx-auto">
             <PageHead />
             <Hero />
 
@@ -54,22 +59,21 @@ const Index = ({ repos, user }) => {
         onAfterOpen={afterOpenModal}
         onRequestClose={closeModal}
         contentLabel="Example Modal"
+       style={customStyles}
       >
         <h2 ref={(_subtitle) => (subtitle = _subtitle)}  className="text-center text-4xl font-extrabold">Repositórios</h2>
-        <div  className="overscroll-contain text-center md:mx-20">
+        <div  className="overscroll-contain text-center md:mx-20 bg-black">
         { show ? repos.map(repo => {
                         return (
-                            <div key={repo.id} className="overscroll-contain bg-gray-200 border px-5 my-3 mx-2 rounded-lg rounded-r-sm">
-                                <p className="break-all text-yellow"><RiCodeBoxFill className="inline-block"/><a href={'https://github.com/'+ repo.full_name}>{repo.full_name}</a> </p>
+                            <div key={repo.id} className="overscroll-contain bg-blue-900 border px-5 my-3 mx-2 rounded-lg rounded-r-sm">
+                                <p className="break-all font-extrabold"><RiCodeBoxFill className="inline-block mx-1"/><a href={'https://github.com/'+ repo.full_name}>{repo.full_name}</a> </p>
                                 {/* <p className="">Language: {repo.language} / <GoStar className="" /> Stars:{repo.stargazers_count}</p> */}
                                 <p className="">Language: {repo.language}</p>
                                 <p>Última atualização: {repo.updated_at}</p>
-                                <p></p>
-                        
                             </div>
                         )
                     }): ""}
-        <button className="text-center w-32 h-8 border-yellow border bg-red-400  rounded-2xl hover:bg-red-200" onClick={closeModal}>close</button>
+        <button className="text-center w-32 h-8 bg-blue-600  border-2 border-blue-600 rounded-full hover:bg-blue-500" onClick={closeModal}>close</button>
         </div>
         
         
@@ -77,13 +81,13 @@ const Index = ({ repos, user }) => {
 
          
             <div className="text-center">
-                <h3 className="text-center text-4xl font-extrabold border-b-2 border-yellow rounded-b-md mx-10 my-5 md:mx-64">Educação</h3>
+                <h3 className="text-center text-4xl font-extrabold bg-blue-600 border-2 border-blue-600 rounded-full  mx-10 my-5 md:mx-64">Educação</h3>
                 <p className="text-xl my-2 ext-left text-justify  mx-5 my-5 md:text-center"><RiDraftLine className="inline-block" /> Análise e Desenvolvimento de Sistemas</p>
                 <p className="text-xl my-2 ext-left text-justify  mx-5 my-5 md:text-center"><RiDraftLine className="inline-block" /> Ensino médio Técnico em Eletrônica</p>
             </div>
 
             <div className="text-center">
-                <h3 className="text-center text-4xl font-extrabold border-b-2 border-yellow rounded-b-md mx-10 my-5 md:mx-64">Conhecimentos</h3>
+                <h3 className="text-center text-4xl font-extrabold  bg-blue-600  border-2 border-blue-600 rounded-full  mx-10 my-5 md:mx-64">Conhecimentos</h3>
                 <p className="text-xl my-2 text-left text-justify mx-5 my-5 md:text-center"><RiBookOpenLine  className="inline-block"/> Conceitos básicos sobre Docker e utilização de containers para criação de APis e micro serviços</p>
                 <p className="text-xl my-2 text-left text-justify mx-5 my-5 md:text-center"><RiBookOpenLine  className="inline-block"/> Conceitos básicos sobre Kubernetes e gerenciamento de containers </p>
                 <p className="text-xl my-2 text-left text-justify mx-5 my-5 md:text-center"><RiBookOpenLine  className="inline-block"/> Criação de Apis utilizando Runtime NodeJS</p>
@@ -96,13 +100,17 @@ const Index = ({ repos, user }) => {
                
             </div>
 
-            <div onClick={openModal} className="text-center border-gray-400 border bg-gray-100 mx-10 rounded-2xl bg-opacity-50 hover:bg-gray-200 md:mx-64">
-            <div className="text-center flex-1">
-            <h3 className="text-center text-4xl font-extrabold border-b-2 border-yellow rounded-b-md mx-10 my-5">Github</h3>
-                <p className="text-center border-yellow border bg-red-400 mx-5 my-5 rounded-2xl">Github stats: <RiGitRepositoryLine  className="inline-block" /> {user.public_repos} / <GoGist  className="inline-block"/> {user.public_gist} / <FiUsers  className="inline-block"/> {user.followers} </p>
+ 
+            <div onClick={openModal} className="mx-10  bg-opacity-50  md:mx-64 bg-footer-texture flex">
+            <div className="flex flex-col  text-center flex-1">
+            <h3 className="text-center text-4xl font-extrabold bg-blue-600 border-2 border-blue-600 rounded-full mx-10 my-5">Github</h3>
+                <div className="flex flex-col  self-center bg-blue-300 border bg-blue-600 mx-5 my-5 p-2 rounded-2xl hover:bg-blue-500 lg:w-1/2">
+                  <p>Github stats: 
+                <RiGitRepositoryLine  className="inline-block" /> {user.public_repos} / <GoGist  className="inline-block"/> {user.public_gist} / <FiUsers  className="inline-block"/> {user.followers} 
+                </p>
+                <RiArrowDownSLine className="self-center text-2xl"/>
+                </div>
             </div>
-
-      
             </div>
 
             <div className="flex-2 justify-center text-center py-1 px-1">
